84fcac5cf404787b0d5705ca4e44e9ecace4e5fc
szaxar <wiktorleczynski@gmail.com>
2018-04-21 23:47:10 +0200

    I added setCellFactory and changed ListView to Application

--- src/main/java/monitoringsystemturbo/presenter/MainPresenter.java
+++ src/main/java/monitoringsystemturbo/presenter/MainPresenter.java
@@ -1,8 +1,11 @@
 package monitoringsystemturbo.presenter;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import monitoringsystemturbo.model.app.Application;
+
 import java.util.List;
 
 
@@ -11,23 +14,46 @@ public class MainPresenter {
 
     private List<Application> loadedApplications;
     @FXML
-    private ListView<String> applicationList;
+    private ListView<Application> applicationList;
+
 
     @FXML
     public void initialize(List<Application> loadedApplications) {
         this.loadedApplications = loadedApplications;
         for (Application application : loadedApplications) {
-            applicationList.getItems().add(application.getName());
+            applicationList.getItems().add(application);
         }
+
+        applicationList.setCellFactory(param -> new ListCell<Application>() {
+            private ImageView imageView = new ImageView();
+
+            @Override
+            public void updateItem(Application name, boolean empty) {
+                super.updateItem(name, empty);
+                //imageView=new ImageView( name.getIcon());
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    setText(name.getName());
+                    //setGraphic(imageView);
+                }
+            }
+        });
+
     }
 
     @FXML
-    public void onAddApplication() {}
+    public void onAddApplication() {
+    }
 
     @FXML
-    public void onRemoveApplication() {}
+    public void onRemoveApplication() {
+    }
 
     @FXML
-    public void onExport() {}
+    public void onExport() {
+    }
+
 
 }
