7da45f7355008b122946012e5e769694fde7aad8
szaxar <wiktorleczynski@gmail.com>
2018-04-22 19:59:43 +0200

    Deleted icon and toString

--- src/main/java/monitoringsystemturbo/model/app/Application.java
+++ src/main/java/monitoringsystemturbo/model/app/Application.java
@@ -1,24 +1,17 @@
 package monitoringsystemturbo.model.app;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileSystemView;
-import java.io.File;
-import java.io.FileNotFoundException;
-
 public class Application {
     private String name;
     private String fullPath;
-    private Icon icon;
 
     public Application() {
         this.name = "";
         this.fullPath = "";
     }
 
-    public Application(String name, String fullPath) throws FileNotFoundException {
+    public Application(String name, String fullPath) {
         this.name = name;
         this.fullPath = fullPath;
-        this.icon = setIcon(fullPath);
     }
 
     public String getName() {
@@ -27,20 +20,5 @@ public class Application {
 
     public String getFullPath() {
         return this.fullPath;
-    }
-
-    private Icon setIcon(String fullPath) throws FileNotFoundException {
-        Icon icon = FileSystemView.getFileSystemView()
-                .getSystemIcon(new File(fullPath));
-        return icon;
-    }
-
-    public Icon getIcon() {
-        return icon;
-    }
-
-    @Override
-    public String toString() {
-        return name;
     }
 }
