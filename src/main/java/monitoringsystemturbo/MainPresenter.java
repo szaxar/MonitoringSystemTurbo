dc4eba5211866d20b53f8a04c75c7058a910e88a
franczak <host9@o2.pl>
2018-04-25 19:09:50 +0200

    Add exporting selected apps statistics

---
+++ src/main/java/monitoringsystemturbo/MainPresenter.java
@@ -0,0 +1,57 @@
+package monitoringsystemturbo.presenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import monitoringsystemturbo.exporter.MainExporter;
+import monitoringsystemturbo.model.TrackingService;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainPresenter {
+    private TrackingService trackingService;
+    private MainExporter mainExporter;
+
+    @FXML
+    private ListView applicationList;
+
+    @FXML
+    public void initialize() {
+        mainExporter = new MainExporter();
+
+        trackingService = new TrackingService();
+        trackingService.addAppToMonitor("idea64");  //just hardcoded for now, we'll change it
+        trackingService.addAppToMonitor("chrome");
+        trackingService.start();
+    }
+
+    @FXML
+    public void onAddApplication() {
+    }
+
+    @FXML
+    public void onRemoveApplication() {
+        //pls remember to save statistics, otherwise data will be lost!
+    }
+
+    @FXML
+    public void onExport() {
+        ExportWindowHandler exportWindowHandler = new ExportWindowHandler();
+        List<String> applicationsToExport = exportWindowHandler.displayCheckingWindow(trackingService.getApplicationsNames());
+        try {
+            mainExporter.export(trackingService, applicationsToExport);
+            Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+            successAlert.setTitle("Success!");
+            successAlert.setHeaderText(null);
+            successAlert.setContentText("Data exported successfully! ");
+            successAlert.showAndWait();
+        } catch (IOException e) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setTitle("Error!");
+            errorAlert.setHeaderText(null);
+            errorAlert.setContentText("Error occurred while exporting data.");
+            errorAlert.showAndWait();
+        }
+    }
+
+}
