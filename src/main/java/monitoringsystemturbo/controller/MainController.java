ecb7229b5cddd4bc647642750261546614b8d36d
szaxar <wiktorleczynski@gmail.com>
2018-04-25 21:20:16 +0200

    change package

--- src/main/java/monitoringsystemturbo/controller/MainController.java
+++ src/main/java/monitoringsystemturbo/controller/MainController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import monitoringsystemturbo.model.ConfigManager;
+import monitoringsystemturbo.config.ConfigManager;
 import monitoringsystemturbo.model.app.Application;
 import monitoringsystemturbo.presenter.MainPresenter;
 import java.io.IOException;
