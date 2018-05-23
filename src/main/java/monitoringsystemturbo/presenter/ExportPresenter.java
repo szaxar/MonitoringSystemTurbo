78707d2fbf88073acfb3fc95f6fba4da742ff044
szaxar <wiktorleczynski@gmail.com>
2018-05-23 20:33:02 +0200

    implementation exporter Presenter

--- src/main/java/monitoringsystemturbo/presenter/ExportPresenter.java
+++ src/main/java/monitoringsystemturbo/presenter/ExportPresenter.java
@@ -1,12 +1,15 @@
 package monitoringsystemturbo.presenter;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
-import monitoringsystemturbo.Main;
 import monitoringsystemturbo.exporter.MainExporter;
 import monitoringsystemturbo.model.TrackingService;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ExportPresenter {
@@ -17,12 +20,38 @@ public class ExportPresenter {
     private TrackingService trackingService;
 
 
+    @FXML
+    private DatePicker fromDatePicker;
+
+    @FXML
+    private DatePicker toDatePicker;
+
+    @FXML
+    private CheckBox allTimeCheckBox;
+
+    @FXML
+    public void initialize(){
+        fromDatePicker.setValue(LocalDate.now());
+        toDatePicker.setValue(LocalDate.now());
+    }
+
+
     public void onConfirm() {
+
+        LocalDate fromDate=fromDatePicker.getValue();
+        LocalDate toDate=toDatePicker.getValue();
+
         ExportWindowHandler exportWindowHandler = new ExportWindowHandler();
         List<String> applicationsToExport = exportWindowHandler.displayCheckingWindow(trackingService.getApplicationsNames());
         if (!exportWindowHandler.getCancelValue()) {
             try {
-                mainExporter.export(trackingService, applicationsToExport);
+
+                if(allTimeCheckBox.isSelected()) {
+                    mainExporter.export(trackingService, applicationsToExport);
+                }
+                else{
+                    mainExporter.export(trackingService, applicationsToExport,fromDate,toDate);
+                }
                 Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
                 successAlert.setTitle("Success!");
                 successAlert.setHeaderText(null);
