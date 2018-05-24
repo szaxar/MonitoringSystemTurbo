6b415d09115b4c9304d6041224c7a52aec5e3cbc
Tomsoonn <tomislawowo@wp.pl>
2018-05-24 12:24:17 +0200

    GUI upgrade

--- src/main/java/monitoringsystemturbo/presenter/AddActivityPresenter.java
+++ src/main/java/monitoringsystemturbo/presenter/AddActivityPresenter.java
@@ -1,27 +1,50 @@
 package monitoringsystemturbo.presenter;
 
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTimePicker;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import monitoringsystemturbo.model.app.Application;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class AddActivityPresenter {
 
     private Stage primaryStage;
+
     @FXML
-    private TextField nameActivity;
+    private JFXDatePicker fromDatePicker;
+
     @FXML
-    private TextField startTime;
+    private JFXDatePicker toDatePicker;
+
     @FXML
-    private TextField duration;
+    private JFXTimePicker toTimePicker;
+
+    @FXML
+    private JFXTimePicker fromTimePicker;
+
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    public void initialize(){
+        fromDatePicker.setValue(LocalDate.now());
+        toDatePicker.setValue(LocalDate.now());
+        fromTimePicker.setValue(LocalTime.of(0,0));
+        toTimePicker.setValue(LocalTime.of(23,59));
+    }
+
+
+    public void onAdd() {
+
+    }
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
-    }
-
-    @FXML
-    public void onAdd() {
     }
 
     public void onCancel() {
