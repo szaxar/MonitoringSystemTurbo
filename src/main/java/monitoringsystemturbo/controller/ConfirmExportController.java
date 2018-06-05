4e089526246b7aa4ff67a53a0e7861be85fa1536
Marcin Habzda <marcin.habzda1@gmail.com>
2018-06-05 14:34:01 +0200

    Refactor export confirm window

---
+++ src/main/java/monitoringsystemturbo/controller/ConfirmExportController.java
@@ -0,0 +1,48 @@
+package monitoringsystemturbo.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import monitoringsystemturbo.presenter.ConfirmExportPresenter;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ConfirmExportController {
+    private Stage primaryStage;
+    private ConfirmExportPresenter confirmExportPresenter;
+
+    public ConfirmExportController(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public List<String> showConfirmationAndGetAppList(List<String> applications)
+            throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(this.getClass().getResource("/confirmExportView.fxml"));
+        Parent rootLayout = loader.load();
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Confirm export");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(primaryStage);
+
+        confirmExportPresenter = loader.getController();
+        confirmExportPresenter.setPrimaryStage(dialogStage);
+        confirmExportPresenter.setApplicationsNames(applications);
+        confirmExportPresenter.start();
+
+        Scene scene = new Scene(rootLayout);
+        dialogStage.setScene(scene);
+        dialogStage.showAndWait();
+
+        return confirmExportPresenter.getApplicationsToExport();
+    }
+
+    public boolean getCancelValue() {
+        return confirmExportPresenter.wasCancelled();
+    }
+}
---
+++ src/main/java/monitoringsystemturbo/presenter/ConfirmExportPresenter.java
@@ -0,0 +1,72 @@
+package monitoringsystemturbo.presenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfirmExportPresenter {
+
+    @FXML
+    private VBox checkBoxesContainer;
+
+    private Stage primaryStage;
+    private List<String> applicationsNames;
+
+    private List<CheckBox> checkBoxes = new ArrayList<>();
+    private List<String> applicationsToExport = new ArrayList<>();
+    private boolean cancelled = true;
+
+    @FXML
+    public void onConfirm() {
+        setSelectedApplications(checkBoxes);
+        cancelled = false;
+        primaryStage.close();
+    }
+
+    @FXML
+    public void onCancel() {
+        primaryStage.close();
+    }
+
+    public void start() {
+        initializeCheckBoxes(applicationsNames);
+        for (CheckBox checkBox : checkBoxes) {
+            checkBoxesContainer.getChildren().add(checkBox);
+        }
+    }
+
+    public List<String> getApplicationsToExport() {
+        return applicationsToExport;
+    }
+
+    public boolean wasCancelled() {
+        return cancelled;
+    }
+
+    public void setApplicationsNames(List<String> applicationsNames) {
+        this.applicationsNames = applicationsNames;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    private void initializeCheckBoxes(List<String> applicationNames) {
+        for (String app : applicationNames) {
+            CheckBox option = new CheckBox(app);
+            option.setSelected(true);
+            checkBoxes.add(option);
+        }
+    }
+
+    private void setSelectedApplications(List<CheckBox> checkBoxes) {
+        for (CheckBox box : checkBoxes)
+            if (box.isSelected()) {
+                applicationsToExport.add(box.getText());
+            }
+    }
+}
